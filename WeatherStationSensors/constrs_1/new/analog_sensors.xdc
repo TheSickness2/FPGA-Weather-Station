ef025ca2045d800644689404eae3df885f47a24e
Christopher Ringhofer <christopher.ringhofer@stud.uni-due.de>
2018-07-13 00:05:29 +0200

    Von Demo-Projekt zu verwendbarer Komponente

--- WeatherStationSensors/constrs_1/new/analog_sensors.xdc
+++ WeatherStationSensors/constrs_1/new/analog_sensors.xdc
@@ -10,7 +10,7 @@ set_property -dict { PACKAGE_PIN R2    IOSTANDARD SSTL135 } [get_ports { CLK100M
 create_clock -add -name sys_clk_pin -period 10.000 -waveform {0 5.000}  [get_ports { CLK100MHZ }];
 
 ## Switches
-set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { pin_select }]; #IO_L20N_T3_A19_15 Sch=sw[0]
+#set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { pin_select }]; #IO_L20N_T3_A19_15 Sch=sw[0]
 #set_property -dict { PACKAGE_PIN H18   IOSTANDARD LVCMOS33 } [get_ports { sw[1] }]; #IO_L21P_T3_DQS_15 Sch=sw[1]
 #set_property -dict { PACKAGE_PIN G18   IOSTANDARD LVCMOS33 } [get_ports { sw[2] }]; #IO_L21N_T3_DQS_A18_15 Sch=sw[2]
 #set_property -dict { PACKAGE_PIN M5    IOSTANDARD SSTL135 } [get_ports { sw[3] }]; #IO_L6N_T0_VREF_34 Sch=sw[3]
@@ -18,18 +18,18 @@ set_property -dict { PACKAGE_PIN H14   IOSTANDARD LVCMOS33 } [get_ports { pin_se
 
 ## RGB LEDs
 #set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { led0_r }]; #IO_L23N_T3_FWE_B_15 Sch=led0_r
-set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { led[4] }]; #IO_L14N_T2_SRCC_15 Sch=led0_g
+#set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { led[4] }]; #IO_L14N_T2_SRCC_15 Sch=led0_g
 #set_property -dict { PACKAGE_PIN F15   IOSTANDARD LVCMOS33 } [get_ports { led0_b }]; #IO_L13N_T2_MRCC_15 Sch=led0_b
 #set_property -dict { PACKAGE_PIN E15   IOSTANDARD LVCMOS33 } [get_ports { led1_r }]; #IO_L15N_T2_DQS_ADV_B_15 Sch=led1_r
-set_property -dict { PACKAGE_PIN F18   IOSTANDARD LVCMOS33 } [get_ports { led[5] }]; #IO_L16P_T2_A28_15 Sch=led1_g
+#set_property -dict { PACKAGE_PIN F18   IOSTANDARD LVCMOS33 } [get_ports { led[5] }]; #IO_L16P_T2_A28_15 Sch=led1_g
 #set_property -dict { PACKAGE_PIN E14   IOSTANDARD LVCMOS33 } [get_ports { led1_b }]; #IO_L15P_T2_DQS_15 Sch=led1_b
 
 
 ## LEDs
-set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { led[0] }]; #IO_L16N_T2_A27_15 Sch=led[2]
-set_property -dict { PACKAGE_PIN F13   IOSTANDARD LVCMOS33 } [get_ports { led[1] }]; #IO_L17P_T2_A26_15 Sch=led[3]
-set_property -dict { PACKAGE_PIN E13   IOSTANDARD LVCMOS33 } [get_ports { led[2] }]; #IO_L17N_T2_A25_15 Sch=led[4]
-set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { led[3] }]; #IO_L18P_T2_A24_15 Sch=led[5]
+#set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { led[0] }]; #IO_L16N_T2_A27_15 Sch=led[2]
+#set_property -dict { PACKAGE_PIN F13   IOSTANDARD LVCMOS33 } [get_ports { led[1] }]; #IO_L17P_T2_A26_15 Sch=led[3]
+#set_property -dict { PACKAGE_PIN E13   IOSTANDARD LVCMOS33 } [get_ports { led[2] }]; #IO_L17N_T2_A25_15 Sch=led[4]
+#set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { led[3] }]; #IO_L18P_T2_A24_15 Sch=led[5]
 
 
 ## Buttons
--- WeatherStationSensors/sources_1/new/analog_sensors.vhd
+++ WeatherStationSensors/sources_1/new/analog_sensors.vhd
@@ -1,16 +1,16 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Christopher Ringhofer
 -- 
 -- Create Date: 11.07.2018 22:25:12
 -- Design Name: 
 -- Module Name: analog_sensors - Behavioral
--- Project Name: 
--- Target Devices: 
+-- Project Name: FPGA Weather Station
+-- Target Devices: Digilent Arty S7-50 (Xilinx Spartan-7)
 -- Tool Versions: 
--- Description: 
+-- Description: Reads out sensor values from A0 and A1 via XADC.
 -- 
--- Dependencies: 
+-- Dependencies: xadc_wiz_0
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -25,16 +25,15 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity analog_sensors is
     PORT (
-        CLK100MHZ : IN STD_LOGIC;
-        vp : IN STD_LOGIC;
+        CLK100MHZ : IN STD_LOGIC;                       -- system clock
+        vp : IN STD_LOGIC;                              -- dedicated analog input (not used)
         vn : IN STD_LOGIC;
-        vauxp0 : IN STD_LOGIC;
+        vauxp0 : IN STD_LOGIC;                          -- auxiliary input 0 (A0)
         vauxn0 : IN STD_LOGIC;
-        vauxp1 : IN STD_LOGIC;
+        vauxp1 : IN STD_LOGIC;                          -- auxiliary input 1 (A1)
         vauxn1 : IN STD_LOGIC;
-        pin_select : IN STD_LOGIC;
-        
-        led : BUFFER STD_LOGIC_VECTOR(5 DOWNTO 0)
+        pin_select : IN STD_LOGIC;                      -- toggles between inputs, '0' for A0 and '1' for A1
+        data_out : BUFFER STD_LOGIC_VECTOR(11 DOWNTO 0) -- 12-bit ADC data output corresponding to pin_select
       );
 end analog_sensors;
 
@@ -64,16 +63,16 @@ COMPONENT xadc_wiz_0
   );
 END COMPONENT;
 
-SIGNAL enable : STD_LOGIC;
-SIGNAL ready : STD_LOGIC;
+SIGNAL enable : STD_LOGIC;                          -- connects end-of-conversion with DRP enable
+SIGNAL ready : STD_LOGIC;                           -- signals for detecting data ready rising edge
 SIGNAL ready_d1 : STD_LOGIC;
 SIGNAL ready_rising : STD_LOGIC;
 SIGNAL ready_falling : STD_LOGIC;
-SIGNAL data : STD_LOGIC_VECTOR(15 DOWNTO 0);
-SIGNAL address_in : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL data : STD_LOGIC_VECTOR(15 DOWNTO 0);        -- ADC value is 12-bits (MSB justified)
+SIGNAL address_in : STD_LOGIC_VECTOR(6 DOWNTO 0);   -- register read address for XADC
 
-CONSTANT a0_address : STD_LOGIC_VECTOR(6 DOWNTO 0) := "0010000";
-CONSTANT a1_address : STD_LOGIC_VECTOR(6 DOWNTO 0) := "0010001";
+CONSTANT a0_address : STD_LOGIC_VECTOR(6 DOWNTO 0) := "0010000";    -- addresses register for A0
+CONSTANT a1_address : STD_LOGIC_VECTOR(6 DOWNTO 0) := "0010001";    -- addresses register for A1
 
 begin
 
@@ -114,44 +113,9 @@ RD_DATA : process(CLK100MHZ)
 begin
     if rising_edge(CLK100MHZ) then
         if ready_rising = '1' then
-        
-            if pin_select = '0' then    -- if we read a photocell, use only the upper bits
-                case data(15 DOWNTO 13) is
-                when "010" =>
-                    led <= "000001";
-                when "011" =>
-                    led <= "000011";
-                when "100" =>
-                    led <= "000111";
-                when "101" =>
-                    led <= "001111";
-                when "110" =>
-                    led <= "011111";
-                when "111" =>
-                    led <= "111111";
-                when others =>
-                    led <= "000000"; 
-                end case;
-            else                        -- if we read a temperature sensor, use lower bits due to lower deviation
-                case data(12 DOWNTO 10) is
-                when "010" =>
-                    led <= "000001";
-                when "011" =>
-                    led <= "000011";
-                when "100" =>
-                    led <= "000111";
-                when "101" =>
-                    led <= "001111";
-                when "110" =>
-                    led <= "011111";
-                when "111" =>
-                    led <= "111111";
-                when others =>
-                    led <= "000000"; 
-                end case;
-            end if;
+            data_out <= data(15 DOWNTO 4);
         else
-            led <= led;
+            data_out <= data_out;
         end if;
     end if;
 end process RD_DATA;
